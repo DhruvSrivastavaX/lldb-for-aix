f14e13eb8bd4706e5aea78605533648b4a1aedb4
Dhruv Srivastava <dhruv.srivastava@ibm.com>
2025-07-18 11:59:22 +0530

    Addressed comment

--- lldb/source/Plugins/Process/aix-core/AIXCore.cpp
+++ lldb/source/Plugins/Process/aix-core/AIXCore.cpp
@@ -105,7 +105,6 @@ bool AIXCore64Header::ParseCoreHeader(lldb_private::DataExtractor &data,
     DataSize = data.GetU64(offset);
 
     *offset += 104;
-    //lldb::offset_t offset_to_user = (*offset + sizeof(ThreadContext64));
     int ret = 0;
     lldb::offset_t offset_to_user = 1216;
     ret = ParseThreadContext(data, offset);
