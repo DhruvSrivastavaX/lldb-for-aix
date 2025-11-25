0973591511befbd5f395dd3912f0bdcb540536c6
DhruvSrivastavaX <dhruv.srivastava@ibm.com>
2025-11-24 22:34:17 -0600

    Fix for delay while quitting debugging session

--- lldb/source/Plugins/Process/AIX/NativeProcessAIX.cpp
+++ lldb/source/Plugins/Process/AIX/NativeProcessAIX.cpp
@@ -953,10 +953,7 @@ Status NativeProcessAIX::Kill() {
     break;
   }
 
-  if (kill(GetID(), SIGKILL) != 0) {
-    error = Status::FromErrno();
-    return error;
-  }
+  error = PtraceWrapper(PT_KILL, GetID(), nullptr, nullptr,  0, nullptr);
 
   return error;
 }
@@ -2061,7 +2058,7 @@ Status NativeProcessAIX::PtraceWrapper(int req, lldb::pid_t pid, void *addr,
         LLDB_LOG(log,"NativeProcessAIX::pthread_sigmask(SIG_UNBLOCK) Failed");
     } else if (req == PT_WATCH) {
       ptrace64(req, pid, (long long)addr, (int)data_size, nullptr);
-    } else if (req == PT_DETACH) {
+    } else if (req == PT_DETACH || req == PT_KILL) {
       ptrace64(req, pid, 0, 0, nullptr);
     } else {
       assert(0 && "Not supported yet.");
