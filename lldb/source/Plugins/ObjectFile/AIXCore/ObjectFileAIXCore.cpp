a8d1ff1a49d846a0564e7b4c02cf93d044edc5ee
DhruvSrivastavaX <dhruv.srivastava@ibm.com>
2026-04-06 13:30:57 -0500

    extractor_sp update for ObjectfileAIXCore

--- lldb/source/Plugins/ObjectFile/AIXCore/ObjectFileAIXCore.cpp
+++ lldb/source/Plugins/ObjectFile/AIXCore/ObjectFileAIXCore.cpp
@@ -73,7 +73,7 @@ ObjectFile *ObjectFileAIXCore::CreateInstance(const lldb::ModuleSP &module_sp,
           extractor_sp = std::make_shared<lldb_private::DataExtractor>(data_sp);
       }
 
-      //assert(data_sp);
+      assert(extractor_sp && extractor_sp->HasData());
 
       // Update the data to contain the entire file if it doesn't already
       if (extractor_sp->GetByteSize() < length) {
@@ -82,12 +82,15 @@ ObjectFile *ObjectFileAIXCore::CreateInstance(const lldb::ModuleSP &module_sp,
               return nullptr;
           data_offset = 0;
           mapped_writable = true;
+          extractor_sp = std::make_shared<lldb_private::DataExtractor>(data_sp);
       }
 
       // If we didn't map the data as writable take ownership of the buffer.
       if (!mapped_writable) {
-          DataBufferSP data_sp = std::make_shared<DataBufferHeap>(data_sp->GetBytes(),
-                  data_sp->GetByteSize());
+          DataBufferSP new_buffer = std::make_shared<DataBufferHeap>(
+              extractor_sp->GetData().GetBytes(),
+              extractor_sp->GetByteSize());
+          extractor_sp = std::make_shared<lldb_private::DataExtractor>(new_buffer);
           data_offset = 0;
       }
 
