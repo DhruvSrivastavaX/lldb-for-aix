8356426ccb95c5b06bb2472c76c81944f79982b3
HemangGadhavi <hemang.gadhavi@ibm.com>
2026-05-26 04:21:52 -0500

    Add fix for next issue with latest compiler

--- lldb/source/Plugins/Instruction/PPC64/EmulateInstructionPPC64.cpp
+++ lldb/source/Plugins/Instruction/PPC64/EmulateInstructionPPC64.cpp
@@ -789,6 +789,16 @@ bool EmulateInstructionPPC64::EmulateB(uint32_t opcode) {
   if (AA) {
     // Absolute addressing: NIA = sign_extend(LI || 0b00)
     next_pc = static_cast<uint64_t>(target_addr);
+
+    // if the absolute target address is a low memory address
+    // (< 0x10000000, typically millicode)
+    // skip the branch and continue to the next instruction instead.
+    if (next_pc < 0x10000000ULL) {
+      LLDB_LOG(log,
+               "EmulateB: Detected bla to low memory address {0:x}, "
+               "skipping to next instruction", next_pc);
+      next_pc = pc_value + 4;
+    }
   } else {
     // Relative addressing: NIA = CIA + sign_extend(LI || 0b00)
     next_pc = pc_value + target_addr;
